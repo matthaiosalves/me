6b4f713345cf6b16b671c032a107144f1a464326
Matheus Alves <lucas24417@gmail.com>
2021-10-06 19:38:11 -0300

    Resolvido

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".navbar-link");
 const progress = document.querySelector(".progress-bars-wrapper");
-const progressBarPercents = [97, 89, 85, 87, 80, 50, 50];
+// const progressBarPercents = [100, 100, 100, 100, 100, 100, 100];
 
 window.addEventListener("scroll", () => {
   mainFn();
@@ -39,3 +39,19 @@ mainFn();
 window.addEventListener("resize", () => {
   window.location.reload();
 });
+
+//contato
+
+const img = document.querySelector("img");
+const icons = document.querySelector(".icons");
+
+img.onclick = function(){
+    this.classList.toggle("active");
+    icons.classList.toggle("active");
+}
+
+function abrirJanela(pagina, largura, altura) {
+    var esquerda = (screen.width - largura)/2;
+    var topo = (screen.height - altura)/2;
+        minhaJanela = window.open(pagina,'','height=' + altura + ', width=' + largura + ', top=' + topo + ', left=' + esquerda);
+}
